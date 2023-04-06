1ce26c50b2f6e8f9dc8f947c4fd3c0499fa6b233
Mihai0290 <mihai.ilie02@yahoo.com>
2023-04-06 12:13:31 +0300

    Bug Fixes

--- mihaiwebsite/settings.py
+++ mihaiwebsite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-irhfk#5n^vpsuzh3x5f+t9!8n5f)np)m!e@%)!&#=v)i(w77qm'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
